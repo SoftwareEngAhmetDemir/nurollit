85d5177e046838d31997b48d650aea75c930e030
Ahmet Demir <43875085+SoftwareEngAhmetDemir@users.noreply.github.com>
2022-04-08 01:13:43 +0300

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,24 @@
+/*
+ *
+ * Application Config
+ *
+ */
+!(function(root, factory) {
+    'use strict';
+
+    if ( typeof define === 'function' && define.amd ) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if ( typeof module !== 'undefined' && module.exports ) {
+        // CommonJS
+        module.exports = factory(root);
+    } else {
+        // Browser globals
+        root.CONFIG = factory(root);
+    }
+}(this, function(/* root */) {
+    'use strict';
+
+    return {
+    };
+}));
